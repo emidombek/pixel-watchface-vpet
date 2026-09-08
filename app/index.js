86741b0742d1a218067d9479269f7e1f547f0f13
emidombek <emilia.maria.dombek@gmail.com>
2026-09-08 09:10:25 +0200

    Set days to linger at adult vpet goal stage

--- app/index.js
+++ app/index.js
@@ -8,3 +8,4 @@ clock.granularity = "minutes";
 const STEP_GOAL = 8000;
 const SPECIES_LIST = ["sprout", "ember", "aqua", "crystal", "shadow", "volt", "bloom", "stone"];
 const STAGE_NAMES = ["egg", "baby", "teen", "adult"];
+const DAYS_TO_LINGER_AT_GOAL = 3;
